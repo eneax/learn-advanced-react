5870ceb89d3ef501eac602194fc5f36e2cd0fa2d
eneax <19818937+eneax@users.noreply.github.com>
2019-02-02 10:27:23 +0100

    Add .gitignore

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a49b714086863bafc417";
+/******/ 	var hotCurrentHash = "4b811556768a96cd9825";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
