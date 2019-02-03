96746c18b6afb35fdcfbaa01cefdafa8725ed1d0
eneax <19818937+eneax@users.noreply.github.com>
2019-02-03 20:16:23 +0100

    First Query and Mutation

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,9 @@
-const Query = {};
+// This is where the db calls are going to go
+const Query = {
+  dogs(parent, args, ctx, info) {
+    global.dogs = global.dogs || [];
+    return global.dogs;
+  },
+};
 
 module.exports = Query;
