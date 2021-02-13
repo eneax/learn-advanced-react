0661a340a90de87925ba17f1adb72a68c69313b0
eneax <19818937+eneax@users.noreply.github.com>
2021-02-13 15:08:32 +0100

    Add nprogress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,12 @@
+import Router from 'next/router';
+import NProgrss from 'nprogress';
+import '../components/styles/nprogress.css';
+
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => NProgrss.start());
+Router.events.on('routeChangeComplete', () => NProgrss.done());
+Router.events.on('routeChangeError', () => NProgrss.done());
 
 const MyApp = ({ Component, pageProps }) => (
   <Page>
