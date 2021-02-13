be6ec57f957537c94cbb895f7e8af18ab2823835
eneax <19818937+eneax@users.noreply.github.com>
2021-02-13 14:43:44 +0100

    Style header with Styled Components

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,17 +1,52 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 
 import Nav from './Nav';
 
+const HeaderStyled = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    border-bottom: 1px solid var(--black, black);
+    display: grid;
+    grid-template-columns: 1fr auto;
+  }
+`;
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
 const Header = () => (
-  <header>
+  <HeaderStyled>
     <div className="bar">
-      <Link href="/">Sick Fits</Link>
+      <Logo>
+        <Link href="/">Sick Fits</Link>
+      </Logo>
     </div>
     <div className="sub-bar">
       <p>Search</p>
     </div>
     <Nav />
-  </header>
+  </HeaderStyled>
 );
 
 export default Header;
