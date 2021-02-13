31c506d9111ea8a18faff0a5c3f0dc4336e21d33
eneax <19818937+eneax@users.noreply.github.com>
2021-02-13 12:03:24 +0100

    Import Head into _document

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,9 +1,10 @@
-import Document, { Html, NextScript, Main } from 'next/document';
+import Document, { Html, Head, NextScript, Main } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en-US">
+        <Head />
         <body>
           <Main />
           <NextScript />
