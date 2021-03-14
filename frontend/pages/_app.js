6992465dd30c3db351af4f895df75f788d6d841b
eneax <19818937+eneax@users.noreply.github.com>
2021-03-14 17:28:46 +0100

    Setting up Apollo Client

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,17 +1,32 @@
+import { ApolloProvider } from '@apollo/client';
+import NProgress from 'nprogress';
 import Router from 'next/router';
-import NProgrss from 'nprogress';
-import '../components/styles/nprogress.css';
-
 import Page from '../components/Page';
+import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
-Router.events.on('routeChangeStart', () => NProgrss.start());
-Router.events.on('routeChangeComplete', () => NProgrss.done());
-Router.events.on('routeChangeError', () => NProgrss.done());
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
-const MyApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
-);
+function MyApp({ Component, pageProps, apollo }) {
+  console.log(apollo);
+  return (
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
+  );
+}
 
-export default MyApp;
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default withData(MyApp);
